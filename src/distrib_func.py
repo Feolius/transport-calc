793e98cabd274fba79cf26386a8bf2f0a818f119
feolius <feolius@gmail.com>
2018-04-17 19:50:07 +0700

    Added inelastic time calculation

--- src/distrib_func.py
+++ src/distrib_func.py
@@ -9,8 +9,11 @@ T = 4.2 * unt.K
 B = 0.4 * unt.T
 n = 7.7 * 10 ** 15 * unt.m ** -2
 m = 0.068 * constants.m_e * unt.kg
+epsilon = 12
+epsilon_0 = constants.epsilon_0 * unt.F / unt.m
+k = 1 / (4 * constants.pi * epsilon_0 * epsilon)
 e = constants.e * unt.C
-k = constants.k * unt.J / unt.K
+kb = constants.k * unt.J / unt.K
 tau_q = 4.7 * unt.ps
 mu = 200 * unt.m ** 2 / (unt.V * unt.s)
 tau_tr = mu * m / e
@@ -25,11 +28,23 @@ dos = m / (constants.pi * hbar ** 2)
 Ef = n / dos
 v_f = (2 * Ef / m) ** 0.5
 
-#@TODO calculate later
-tau_in = 0.1 * tau_tr.number() * tau_tr.unit()
-Q_dc = 2 * (tau_tr / tau_in) * (e * E_dc * v_f / w_c) ** 2 * (constants.pi / (hbar * w_c)) ** 2
-Q_dc = Q_dc.cast_unit(unt.unitless)
+# Calculation of Q_dc and tau inelastic here
+kappa = 2 * constants.pi * e ** 2 * dos * k
+tau_in_log_arg = kappa * v_f / (w_c * (w_c * tau_tr) ** 0.5)
+tau_in_log_arg = tau_in_log_arg.cast_unit(unt.unitless)
+tau_in_log_arg = tau_in_log_arg.number()
+tau_in_log = np.log(tau_in_log_arg)
+def tau_in(E):
+    tau_in_inv = (((constants.pi * kb * T) ** 2 + E ** 2) / (4 * constants.pi * Ef * hbar)) * tau_in_log
+    return tau_in_inv ** -1
+Q_dc_prefix = 2 * tau_tr * (e * E_dc * v_f / w_c) ** 2 * (constants.pi / (hbar * w_c)) ** 2
+def Q_dc(E):
+    Q_dc = Q_dc_prefix / tau_in(E)
+    Q_dc = Q_dc.cast_unit(unt.unitless)
+    return Q_dc
 
+
+# Dingle factor
 dingle = - constants.pi / (w_c * tau_q)
 dingle = dingle.cast_unit(unt.unitless)
 dingle = np.exp(dingle.number())
@@ -45,7 +60,7 @@ dE = dE.cast_unit(meV)
 
 
 def fermi_dirac(E, Ef, T):
-    exp_arg = (E - Ef)/(k*T)
+    exp_arg = (E - Ef)/(kb*T)
     exp_arg = exp_arg.cast_unit(unt.unitless)
     exp_arg = exp_arg.number()
     return 1.0 / (np.exp(exp_arg) + 1)
@@ -58,16 +73,14 @@ def fermi_dirac_wrapper(E):
 def fermi_dirac_der_wrapper(E):
     fermi_dirac_der = (fermi_dirac(E + dE, Ef, T) - fermi_dirac(E, Ef, T)) / dE
     return fermi_dirac_der.number()
-
-
+f_osc_sin_prefix = 8 * constants.pi / (hbar * w_c)
 def f_osc_sin_arg(E):
-    arg = 2 * constants.pi * E / (hbar * w_c) * (4 * Q_dc / (1 + Q_dc))
+    arg = f_osc_sin_prefix * E * Q_dc(E) / (1 + Q_dc(E))
     return arg.cast_unit(unt.unitless).number()
 
 
 f_osc_arg = np.linspace(-5, 5, 1000)
 
-# x = np.linspace(0, 2 * Ef.number(), 1000) * Ef.unit()
 f_0 = map(lambda arg: fermi_dirac_wrapper(f_osc_arg_get_E(arg)), f_osc_arg)
 f_0_der = map(lambda arg: fermi_dirac_der_wrapper(f_osc_arg_get_E(arg)), f_osc_arg)
 f_osc_sin = map(lambda arg: np.sin(f_osc_sin_arg(f_osc_arg_get_E(arg))), f_osc_arg)
@@ -77,7 +90,6 @@ f_osc = f_osc_prefix * np.multiply(f_0_der, f_osc_sin)
 f = f_osc + f_0
 
 
-# x = map(lambda x: x.number(), x)
 plt.plot(f_osc_arg, f)
 plt.plot(f_osc_arg, f_0)
 plt.show()
