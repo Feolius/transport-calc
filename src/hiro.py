91eea1a71cc52ae2c3b6684b87eb8bf0ff99180b
feolius <feolius@gmail.com>
2018-06-03 23:54:53 +0700

    more hiro calc code

--- src/hiro.py
+++ src/hiro.py
@@ -38,11 +38,27 @@ def V_B_per_V_0_wrapper(B):
 #
 # V_B = map(V_B_per_V_0_wrapper, data_cut[0]* unt.T)
 # plt.plot(data_cut[0], V_B)
-inv_data = data_cut[0] ** -1
-arg = np.linspace(inv_data[0], inv_data[-1], 3000)
-trf = np.interp(arg, inv_data, data_cut[1])
-plt.plot(inv_data, data_cut[1], 'o')
-plt.plot(arg, trf)
+inv_data = (np.flip(data_cut[0] ** -1, 0), np.flip(data_cut[1], 0))
+interp_arg = np.linspace(inv_data[0][0], inv_data[0][-1], 4000)
+interp_val = np.interp(interp_arg, inv_data[0], inv_data[1])
+interp_data = (interp_arg, interp_val)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(inv_data[0], inv_data[1])
+ax.plot(interp_data[0], interp_data[1])
+ax.set_picker(True)
+def onclick(event):
+    print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
+        event.button, event.x, event.y, event.xdata, event.ydata)
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+def handle_close(evt):
+    print('Closed Figure!')
+
+fig.canvas.mpl_connect('close_event', handle_close)
+
+
 # freq = rfft(data_cut[1])
 # plt.plot(freq)
 plt.show()
